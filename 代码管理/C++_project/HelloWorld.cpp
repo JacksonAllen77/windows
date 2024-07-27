d5529304d4b46c894f3e8ba13f3efb7988424382
PaulSith <928644549@qq.com>
2024-07-27 20:54:15 +0800

    2024/7/27（20点53分）

--- 代码管理/C++_project/HelloWorld.cpp
+++ 代码管理/C++_project/HelloWorld.cpp
@@ -1,18 +1,9 @@
 #include <iostream>
 using namespace std;
-#include<string>
-#include<vector>//使用list容器所需要的头文件
-#include<map>//使用map&multimap所需要的头文件
-#include<algorithm>//使用算法所需要的头文件
-#include <ctime>   // 使用time所需要的头文件
 
-void test01()
-{
-    
-}
 int main()
 {
-    test01();
+    cout << "Hello World" << endl;
     system("pause");
     return 0;
 }
